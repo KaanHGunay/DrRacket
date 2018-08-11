9fbda5219a3a510690680fa6a1bfff3b1e5aace4
KaanHGunay <kaangunay16@gmail.com>
2018-08-11 18:53:41 +0300

    İlkCommit

---
+++ 01_Ilk.rkt
@@ -0,0 +1,14 @@
+#lang racket
+(+ 2 4)
+(+ (+ 5 10) (+ 4 9))
+(sin 0)
+(define x 3)
+(define y 4)
+(define z (+ (* x x) (* y y)))
+(sqrt z)
+
+"Kaan Günay"
+(define isim "Kaan Günay")
+isim
+
+(string-append isim "___")
